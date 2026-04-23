9ea9cb051614a08123d820b519c29875b2fb8192
Kliodt <vadimkl2004@mail.ru>
2026-04-24 00:52:18 +0300

    rm dot from title, fix scroll again

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,29 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/common.css";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Navigate,
+  Outlet,
   Route,
   RouterProvider,
-  useLocation,
+  ScrollRestoration,
 } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import PeoplePage from "./pages/PeoplePage";
 import { Api } from "./api";
-import React, { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import Background from "./components/Background";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Background from "./components/Background";
-
-// Фикс для сброса скролла при перехода между страницами
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-    window.scrollTo({top: 0, left: 0, behavior: 'instant'});
-  }, [pathname]);
-
-  return null;
-}
+import HomePage from "./pages/HomePage";
+import PeoplePage from "./pages/PeoplePage";
+import "./styles/common.css";
 
 function App() {
   return (
     <>
       <Header />
       <Background />
-      <ScrollToTop />
       <Outlet />
+      <ScrollRestoration />
       <Footer />
     </>
   );
