0d22a6e2d6ff484188fc04ac479948808b54278b
Kliodt <vadimkl2004@mail.ru>
2026-04-23 05:04:37 +0300

    try to fix GH pages routes bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/common.css";
+import { Api } from "./api";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={Api.normalizeURL('')}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
